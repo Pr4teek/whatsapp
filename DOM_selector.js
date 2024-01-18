c5bace5c3ec0697cbc6c0fd97d01cbe3af4b7c09
Pr4teek <132746913+Pr4teek@users.noreply.github.com>
2024-01-18 14:19:10 +0530

    Delete DOM_selector.js

--- DOM_selector.js
+++
@@ -1,8 +0,0 @@
-const mess = 'bhumika gets angry in faltu... :)';
-
-document.getElementById('whatsapp_share').addEventListener('click', ()=> {
-    let whatsappUrl = `whatsapp://send?text= ${encodeURIComponent(mess)}`; 
-    window.location.href =  whatsappUrl;
-});
-
-
